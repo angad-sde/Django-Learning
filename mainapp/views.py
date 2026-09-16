118b58476ea72a598481aa25f7c1561adb4b4712
Angad <angadkaur1015@gmail.com>
2026-09-16 23:30:35 +0530

    view added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def simple_hello(request):
+    return HttpResponse("<h1>Hello, world! You are at the index page.</h1>")
+
+def home_view(request):
+    context = {
+        "title": "Welcome to My Site",
+        "items": ["Python", "Django", "Web Development"],
+    }
+    
+    return render(request, "myapp/home.html", context)
