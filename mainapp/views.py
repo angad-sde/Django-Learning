fd24945b660cdb1f890c37cc69feb17ea55feca5
Angad <angadkaur1015@gmail.com>
2026-09-17 23:38:20 +0530

    view and model added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
 from django.http import HttpResponse
 
-def simple_hello(request):
-    return HttpResponse("<h1>Hello, world! You are at the index page.</h1>")
 
-def home_view(request):
-    context = {
-        "title": "Welcome to My Site",
-        "items": ["Python", "Django", "Web Development"],
-    }
-    
-    return render(request, "myapp/home.html", context)
+# READ: Show all students
+def student_list(request):
+    students = Student.objects.all()
+
+    return render(
+        request,
+        "students/student_list.html",
+        {"students: students"}
+    )
+
+def student_create(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+
+        Student.objects.create(
+            name = name,
+            email = email,
+            age = age
+        )
+
+        return redirect("student_list")
+
+    return render(request, "students/student_form.html")
+
+# READ: show one student
+def student_detail(request,id):
+    student = get_object_or_404(Student, id=id)
+
+    return render(
+        request,
+        "students/student_detail.html",
+        {"student": student}
+    )
+
+# UPADTE: edit an existing student
+def student_update(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if(request.method == "POST"):
+        student.name = request.POST.get
